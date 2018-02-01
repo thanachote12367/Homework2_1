be69c4e4e5927e26a6e14306801af387b5f5b796
thanachote12367 <35685006+thanachote12367@users.noreply.github.com>
2018-02-01 21:17:25 +0700

    Add files via upload

---
+++ hw21.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from datetime import datetime, date
+
+app = Flask(__name__)
+
+def calculate_age(born):
+    today = date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+@app.route("/age", methods = ['POST'])
+def timestamp():
+    if request.method == 'POST':
+        try:
+            day = request.form.get('day')
+            print(day)
+            date_of_birth = datetime.strptime(day, "%d-%m-%Y")
+            print(date_of_birth)
+            return jsonify({ 'birthday': day, 'age': calculate_age(date_of_birth)})
+            pass
+        except Exception as e:
+            return jsonify({ 'birthday': str(e), 'age': str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug = True, port=5000)
